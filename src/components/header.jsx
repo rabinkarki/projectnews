3499eb48a0dc6f43849eddd2bece677d659ae20d
Rabin karki <rabinkarki970@gmail.com>
2023-08-21 09:10:44 +0545

    loginfunc

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,32 @@
 import React from "react";
 import { useGlobalContext } from "./context";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
   const { query, Searchquery } = useGlobalContext();
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
   return (
     <>
-      <h1>Tech News On The go</h1>
+      <div className="head-1">
+        <h1>Tech News On The go</h1>
+        <div>
+          {isAuthenticated ? (
+            <li>
+              <button
+                onClick={() =>
+                  logout({ logoutParams: { returnTo: window.location.origin } })
+                }
+              >
+                LogOut
+              </button>
+            </li>
+          ) : (
+            <li>
+              <button onClick={() => loginWithRedirect()}>Log In</button>
+            </li>
+          )}
+        </div>
+      </div>
       <form onSubmit={(e) => e.preventDefault()}>
         <div>
           <input
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AppProvider } from './components/context.jsx'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Auth0Provider
+    domain="dev-j6uogsku5ivtd35l.us.auth0.com"
+    clientId="6aobNyOH9wteOXc4NYCY0K5hZEyKffIB"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >    
   <AppProvider>
     <App />
-  </AppProvider>,
+  </AppProvider>
+  </Auth0Provider>,
 )
